9f55cd086b45e13c702c00cf4dca7534e91fb583
Abhishek Kaundal <144792445+AbhishekKaundal0052@users.noreply.github.com>
2023-12-06 02:45:00 +0530

    Add files via upload

---
+++ PROJECT 4.2/Fill.asm
@@ -0,0 +1,65 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input.
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed. 
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
+
+// Put your code here.
+
+(RESET)     
+@SCREEN
+D = A       // base screen map address
+@address
+M = D       // initiate address = base address
+
+(CHECK)     //check if keys have been pressed
+@KBD
+D = M
+@BLACK      // if yes, go-to black
+D;JGT	
+@WHITE      // if no, go-to white
+D;JEQ	
+
+(BLACK)     // black screen
+@whatToDo
+M = -1	
+
+@LOOP
+0;JMP
+
+(WHITE)     // white screen
+@whatToDo
+M = 0	
+
+@LOOP
+0;JMP
+
+(LOOP)   
+@whatToDo	
+D = M	   
+
+@address
+A = M	
+M = D	
+
+@address
+D = M+1	  
+
+@KBD
+D = A-D	    
+@address
+M = M+1	       
+
+@LOOP
+D;JGT	   
+
+@RESET
+0;JMP
+   
